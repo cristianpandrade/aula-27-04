6ae6459a4a16762a83e7642e40834d4e026a1dd5
ericpanitz <ericpanitz2004@gmail.com>
2023-04-27 21:29:57 -0300

    first commit

---
+++ aula 27/04/index.js
@@ -0,0 +1,35 @@
+//const name = prompt("insira seu nome")
+//const color = prompt("insira sua idade")
+
+//Frase de teste
+//console.log("A cor favorita de"+ name +" é" +  color)
+
+//console.log(`seu nome é ${name} e cor favorita é ${color}`)
+
+//Transfomar letras maiúsculas em minúsculo
+//console.log(name.toLowerCase())
+//console.log(name.toUpperCase())
+
+//E-mails
+//const name ="Crisitan pereira"
+//console.log(name.trim());
+
+//Correção de maiúsculo para minúsculo
+
+//const receita = "bolo de abacaxi"
+//const novaReceita =receita.replace("abacaxi", "cenoura")
+
+//console.log(novaReceita);
+
+//console.log(receita.trim().toLocaleLowerCase().includes("cenoura"));
+
+//Exercicio, transformar uma frase em maiúsculo
+//Substituir a vogal "o" por "i"
+//Indicar o tamanho da frase
+
+//const frase = prompt("Digite uma frase")
+//const vogal =frase.replaceAll("o", "i")
+
+
+//console.log(vogal.toUpperCase())
+//console.log(vogal.length)
